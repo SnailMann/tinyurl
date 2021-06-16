3de509fc4e65e314c817a4e134789420989a641e
SnailMann <453001674@qq.com>
2021-06-16 19:26:27 +0800

    init framework

---
+++ shortchain-common/src/main/java/com/snailmann/shortchain/common/ShortchainCommonAutoConfiguration.java
@@ -0,0 +1,13 @@
+package com.snailmann.shortchain.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortchainCommonAutoConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortchainCommonAutoConfig.class, args);
+    }
+
+}
---
+++ shortchain-server/src/main/java/com/snailmann/shortchain/server/ShortchainServerApplication.java
@@ -0,0 +1,16 @@
+package com.snailmann.shortchain.shortchainserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liwenjie
+ */
+@SpringBootApplication
+public class ShortchainServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortchainServerApplication.class, args);
+    }
+
+}
